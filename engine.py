0a2d89cc7b6dc09740e3ad08fb2bd6054cb4e5df
Yoga <zach.groenewald@gmail.com>
2020-06-04 14:52:06 +0200

    Part 1.1
    - Refer to instance of console instead of directly to default

--- engine.py
+++ engine.py
@@ -18,6 +18,9 @@ def main():
     libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GRAYSCALE | libtcod.FONT_LAYOUT_TCOD)
     libtcod.console_init_root(screen_width, screen_height, 'RLDev2020')
 
+    # make default console
+    con = libtcod.console_new(screen_width, screen_height)
+
     key = libtcod.Key()
     mouse = libtcod.Mouse()
 
@@ -25,13 +28,16 @@ def main():
     while not libtcod.console_is_window_closed():
 
         # print to screen
-        libtcod.console_clear(0)
-        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_clear(con)
+        libtcod.console_set_default_foreground(con, libtcod.white)
 
         # this draws the @
-        libtcod.console_put_char(
-            0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
         libtcod.console_flush()
+
+        # FIXME: Why is this line here?
+        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
 
         # get inputs
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
